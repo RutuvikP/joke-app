0ed7b07e3127f5d13fb8a8bf8c51d0b7738d9e79
Rutuvik Patil <you@example.com>
2023-06-29 00:42:54 +0530

    First Commit

---
+++ frontend/src/Pages/QuoteGenerator.jsx
@@ -0,0 +1,49 @@
+import React, { useState } from 'react';
+import './Quote.css';
+import axios from 'axios';
+
+const QuoteGenerator = () => {
+  const [keyword, setKeyword] = useState('');
+  const [quote, setQuote] = useState('');
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState('');
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    setIsLoading(true);
+    setError('');
+
+    try {
+      const response = await axios.get(`http://localhost:8000/quote?keyword=${keyword}`);
+      console.log(response);
+      setQuote(response.data);
+    } catch (error) {
+      setError('Failed to generate quote. Please try again.');
+    }
+
+    setIsLoading(false);
+  };
+
+  return (
+    <div className="quote-generator">
+      <form onSubmit={handleSubmit} className="quote-generator__form">
+        <input
+          type="text"
+          value={keyword}
+          onChange={(e) => setKeyword(e.target.value)}
+          placeholder="Enter a keyword"
+          className="quote-generator__input"
+        />
+        <button type="submit" disabled={isLoading} className="quote-generator__button">
+          {isLoading ? 'Generating...' : 'Generate Quote'}
+        </button>
+      </form>
+
+      {quote && <p className="quote-generator__quote">Generated Quote: {quote}</p>}
+      {error && <p className="quote-generator__error">{error}</p>}
+    </div>
+  );
+};
+
+export default QuoteGenerator;
